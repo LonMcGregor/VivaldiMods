38d7b4b4c6f56df095601131c46212c6de811e43
Léon McGregor <lonmcgregor@outlook.com>
2016-05-05 22:57:23 +0100

    Reference Proper Directories
    Assumes repo is at %LOCALAPPDATA%/Vivaldi/Application

--- customstyles.bat
+++ customstyles.bat
@@ -1,3 +1,3 @@
 @echo off
-FOR /D %%i IN (1.*) DO copy browser.html %%i\resources\vivaldi\browser.html /Y
-FOR /D %%i IN (1.*) DO copy mystyles.css %%i\resources\vivaldi\style\mystyles.css /Y
+FOR /D %%i IN (..\1.*) DO copy browser.html %%i\resources\vivaldi\browser.html /Y
+FOR /D %%i IN (..\1.*) DO copy mystyles.css %%i\resources\vivaldi\style\mystyles.css /Y
