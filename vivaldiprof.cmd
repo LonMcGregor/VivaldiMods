98dd02579c85029c94a82c1a9eba2f1520eb0c0f
Léon McGregor <lonmcgregor@outlook.com>
2017-07-11 21:19:50 +0100

    Remove GUI profile manager, improve CLI

--- vivaldiprof.cmd
+++ vivaldiprof.cmd
@@ -1,6 +1,9 @@
 @echo off
+color 4f
+mode con:cols=80 lines=1
 set profile=%1
 :set
+cls
 if [%profile%]==[] set /p profile="Enter profile Name: "=
 if [%profile%]==[] goto set
 start %LOCALAPPDATA%\Vivaldi\Application\vivaldi.exe --user-data-dir=%LOCALAPPDATA%\Vivaldi\%profile% --flag-switches-begin --debug-packed-apps --silent-debugger-extension-api --flag-switches-end
