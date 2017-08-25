53c7240d0b3a6d2435ca3d4b75c39499c366bfd2
Léon McGregor <lonmcgregor@outlook.com>
2017-08-25 20:22:12 +0100

    New way of managing vivaldi mod installing

---
+++ custom/fixIcons.js
@@ -0,0 +1,22 @@
+setInterval(function () {
+    // Test if panels have been loaded in yet
+    var panelSwitch = document.querySelector("#switch");
+	if (panelSwitch != null) {
+        // Check all panel buttons
+        var buttons = document.getElementById("switch").children;
+		for(var i = 0; i < buttons.length; i++) {
+            // If they are a user added web view
+            if (buttons[i].className.indexOf("webviewbtn") >= 0){
+                var icon = buttons[i].children[0];
+                // If they have the wrong protocol
+                if (icon.src.substring(0, 24) === "http://chrome-extension/"){
+                    // Replace with correct protocol
+                    icon.src = "chrome-extension://" + icon.src.substring(24);
+                }
+            }
+        }
+    }
+    // Repeat on sparse interval as I suspect bug
+    // manifests whenever there is a change of icon
+    // as made by the extension background task
+}, 2000);
