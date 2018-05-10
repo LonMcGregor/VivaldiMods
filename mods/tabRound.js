287ad65ec37e3ef4d2e81bc0d29a6651411296d6
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-05-10 19:18:44 +0100

    improve tabround

--- mods/tabRound.js
+++ mods/tabRound.js
@@ -1,12 +1,16 @@
-var CLIP_PATH_D = 'M 0,31 C 12.284275,31 17.311144,22.728149 18.160676,14.235439 19.010208,5.7427293 24.864826,0 34,0 v 31 z';
+(function tabRoundMod(){
+    "use strict";
+    var CLIP_PATH_D = 'M 0,31 C 12.284275,31 17.311144,22.728149 18.160676,14.235439 19.010208,5.7427293 24.864826,0 34,0 v 31 z';
 
-setTimeout(function makeSvg(){
-    var footer = document.querySelector("#footer");
-    if (footer != null) {
-        var span = document.createElement('span');
-        span.innerHTML = '<svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><defs><clipPath id="tab_rounder_clip"><path d="'+CLIP_PATH_D+'"></path></clipPath></defs></svg>';
-        footer.appendChild(span);
-    } else {
-        setTimeout(makeSvg, 500);
-    }
-}, 500)
+    setTimeout(function makeSvg(){
+        var footer = document.querySelector("#footer");
+        if (footer != null) {
+            var span = document.createElement('span');
+            span.innerHTML = '<svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><defs><clipPath id="tab_rounder_clip"><path d="'+CLIP_PATH_D+'"></path></clipPath></defs></svg>';
+            footer.appendChild(span);
+        } else {
+            setTimeout(makeSvg, 500);
+        }
+    }, 500)
+    
+})();
