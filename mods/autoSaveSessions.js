3602031b58cf2f232834fba65657ab54df7c541f
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-12-14 11:08:00 +0000

    sessions autosave allow multi-window and use interval

--- mods/autoSaveSessions.js
+++ mods/autoSaveSessions.js
@@ -26,7 +26,7 @@ const SESSION_AUTOSAVE_DELAY = SESSION_AUTOSAVE_DELAY_MINUTES * 1000 * 60;
 *     Oldest sessions are deleted if there are too many
 *     Set to Infinity to keep all sessions
 */
-const SESSION_AUTOSAVE_MAX_OLD_SESSIONS = 4;
+const SESSION_AUTOSAVE_MAX_OLD_SESSIONS = 5;
 
 /**
  * Variable validations
@@ -49,25 +49,41 @@ if(SESSION_AUTOSAVE_MAX_OLD_SESSIONS <= 0){
  */
 function autoSaveSession(){
     vivaldi.sessionsPrivate.getAll(allSessions => {
+        const now = new Date();
         const autosavesOnly = allSessions.filter(x => x.name.indexOf(SESSION_AUTOSAVE_PREFIX)===0);
         const oldestFirst = autosavesOnly.sort((a,b) => {return a.createDateJS - b.createDateJS;});
-        /* length + 1 as we are about to add a new one */
-        if(oldestFirst.length + 1 > SESSION_AUTOSAVE_MAX_OLD_SESSIONS){
-            vivaldi.sessionsPrivate.delete(oldestFirst[0].name,() => {});
+
+        /* check that the most recent session is older than the delay
+        If there are multiple windows open this might happen */
+        if(oldestFirst.length > 0){
+            const oldest = oldestFirst[oldestFirst.length-1];
+            const delay = now - oldest.createDateJS;
+            if(delay < SESSION_AUTOSAVE_DELAY){
+                return;
+            }
         }
-        const name = SESSION_AUTOSAVE_PREFIX + new Date().toISOString().replace(":",".").replace(":",".");
+
+        /* delete older sessions */
+        let numberOfSessions = oldestFirst.length + 1; /* length + 1 as we are about to add a new one */
+        let oldestIndex = 0;
+        while(numberOfSessions > SESSION_AUTOSAVE_MAX_OLD_SESSIONS){
+            vivaldi.sessionsPrivate.delete(oldestFirst[oldestIndex].name,() => {});
+            oldestIndex++;
+            numberOfSessions--;
+        }
+
+        /* create the new session */
+        const name = SESSION_AUTOSAVE_PREFIX + now.toISOString().replace(":",".").replace(":",".");
         const options = {
             saveOnlyWindowId: 0
         };
-        vivaldi.sessionsPrivate.saveOpenTabs(name, options, () => {
-            setTimeout(autoSaveSession, SESSION_AUTOSAVE_DELAY);
-        });
+        vivaldi.sessionsPrivate.saveOpenTabs(name, options, () => {});
     });
 }
 
 /**
  * Init the mod
  */
-setTimeout(autoSaveSession, SESSION_AUTOSAVE_DELAY);
+setInterval(autoSaveSession, SESSION_AUTOSAVE_DELAY);
 
 })();
