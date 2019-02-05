5e4efbbf84a14f33db487f6a68d9664b7cba0d1e
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-02-05 21:16:17 +0000

    Media marquee, attempt 2 for feature #12

--- mods/mediaMarquee.js
+++ mods/mediaMarquee.js
@@ -6,25 +6,45 @@
  */
 
 (function mediaMarquee() {
-    "use strict";
+   "use strict";
+
+   /**
+    * Change the marquee speed
+    *    Lower is faster
+    *    Default: 1
+    */
+   const MARQUEE_SPEED = 0.75;
 
    /**
     * Update the title in the marquee of a tab
+    * Need to check type of audible, as if it is false,
+    *    if() will be false, but we still want to update
     * @param {int} tabId
     * @param {chrome.tabs.changeInfo} changeInfo
     * @param {chrome.tabs.Tab} tabInfo
     */
    function tabTitleChanged(tabId, changeInfo, tabInfo){
-      if(changeInfo.title || changeInfo.audible || changeInfo.mutedInfo){
+      if(changeInfo.title || typeof(changeInfo.audible)===typeof(true) || changeInfo.mutedInfo){
          const marquee = document.querySelector("#lonm-mediaMarquee-"+tabId);
          if(marquee){
-            console.log("Updated tab " + tabId + "title: "+ tabInfo.title);
-            marquee.innerText = tabInfo.title;
-            marquee.start();
-         } else {
+            setMarqueeContent(tabInfo);
+         } else if(tabInfo.audible) {
             createMarquee(tabInfo);
          }
       }
+   }
+
+   /**
+    * Update the style and text of a marquee for a specific tab
+    *    ASSUME that the marquee for a tab has already been added
+    * @param {chrome.tabs.Tab} tabInfo
+    */
+   function setMarqueeContent(tabInfo){
+      const marquee = document.querySelector("#lonm-mediaMarquee-"+tabInfo.id);
+      const speed = (tabInfo.title.length * MARQUEE_SPEED) + "s";
+      marquee.innerText = tabInfo.title;
+      marquee.style.animationDuration = speed;
+      console.log("Updated tab " + tabInfo.id + " title: "+ tabInfo.title + " at speed " + speed);
    }
 
    /**
@@ -33,15 +53,15 @@
     */
    function createMarquee(tabInfo){
       const tab = document.querySelector("#tab-"+tabInfo.id);
-      const marquee = document.createElement("marquee");
-      marquee.innerText = tabInfo.title;
+      const marqueeOuter = document.createElement("span");
+      marqueeOuter.className = "lonm-mediaMarqueeOuter";
+      const marquee = document.createElement("span");
       marquee.id = "lonm-mediaMarquee-"+tabInfo.id;
-      marquee.className = "lonm-mediaMarquee";
-      marquee.setAttribute("scrollamount", "4");
-      marquee.setAttribute("loop", "-1");
-      tab.querySelector(".tab-header").appendChild(marquee);
+      marquee.className = "lonm-mediaMarqueeInner";
+      marqueeOuter.appendChild(marquee);
+      tab.querySelector(".tab-header").appendChild(marqueeOuter);
       console.log("Created tab" + tabInfo.id + "marquee: "+ tabInfo.title);
-      marquee.start();
+      setMarqueeContent(tabInfo);
    }
 
    chrome.tabs.onUpdated.addListener(tabTitleChanged);
