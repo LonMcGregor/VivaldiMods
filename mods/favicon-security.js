6da1aad6d78d8d612ce7e10713df41f46eb131fc
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-02-10 17:31:11 +0000

    fix #9

--- mods/favicon-security.js
+++ mods/favicon-security.js
@@ -6,82 +6,70 @@
 * Assumes presence of both the tab bar and the address bar
 */
 
-// Constant CSS Selectors
-const SECURITY_ZONE_ICON = "div.addressfield > button.button-addressfield.addressfield-siteinfo > div > svg";
-const ACTIVE_TAB = ".tab.active .favicon";
-const SECURITY_ZONE = "div.addressfield > button.button-addressfield.addressfield-siteinfo > div.siteinfo-symbol";
+(function faviconSecurity(){
+    "use strict";
 
-// Selectors and config for observers
-const BROWSER_STATE_INDICATOR = "#browser"
-const BROWSER_STATE_INDICATOR_CONFIG = {attributes: true}
-const TAB_CHANGE_INDICATOR = ".tab-strip";
-const TAB_CHANGE_INDICATOR_CONFIG = {attributes: true, subtree: true}
+    // Constant CSS Selectors
+    const SECURITY_ZONE_ICON = "div.addressfield > button.button-addressfield.addressfield-siteinfo > div > svg";
+    const ACTIVE_TAB = ".tab.active .favicon";
+    const SECURITY_ZONE = "div.addressfield > button.button-addressfield.addressfield-siteinfo > div.siteinfo-symbol";
 
-// Observer for changes to tab and browser state
-const browserStateChangeObserver = new MutationObserver(browser_state_changed)
-const differentTabActivatedObserver = new MutationObserver(tab_changed);
-
-// The browser changed state,
-// May need to re-observe deleted elements
-// E.g. if enter and exit of fullscreen / hide UI mode
-function browser_state_changed(m, o){
-    //console.log("BROWSER STATE CHANGE");
-    remove_existing_cloned_favicon();
-    clone_favicon_and_add_to_security();
-    differentTabActivatedObserver.disconnect();
-    browserStateChangeObserver.disconnect();
-    load_favicon_security_mod();
-}
-
-// The tab was changed, so favicon needs changing
-// this will cover changes to the .active tab AND
-// cover changes to the favicon span thru subtree
-function tab_changed(m, o){
-    //console.log("TAB STRIP ATTRIBUTE CHANGE");
-    clone_favicon_and_add_to_security();
-}
-
-// Clone the favicon and add a copy to the security zone
-// also style it appropriately
-function clone_favicon_and_add_to_security(){
-    // make sure everything we need to access exists
-    const sz_icon = document.querySelector(SECURITY_ZONE_ICON);
-    const favicon_original = document.querySelector(ACTIVE_TAB);
-    const siteinfo = document.querySelector(SECURITY_ZONE);
-    if(!favicon_original || !sz_icon || !siteinfo){return;}
-    remove_existing_cloned_favicon();
-    // copy, style and set the favicon
-    // use deep clone to also copy svg icons
-    const favicon = favicon_original.cloneNode(true);
-    favicon.style.width = "16px";
-    favicon.style.height = "16px";
-    favicon.style.display = "block";
-    favicon.style.margin = "3px";
-    favicon.style.backgroundSize = "contain";
-    siteinfo.style.display = "flex";
-    sz_icon.parentNode.insertBefore(favicon, sz_icon);
-}
-
-// Remove any existing cloned favicon
-function remove_existing_cloned_favicon(){
-    const old_sz_favicon = document.querySelector(SECURITY_ZONE+" .favicon");
-    if(old_sz_favicon){
-        old_sz_favicon.parentElement.removeChild(old_sz_favicon);
+    // Clone the favicon and add a copy to the security zone
+    // also style it appropriately
+    function clone_favicon_and_add_to_security(){
+        // make sure everything we need to access exists
+        const sz_icon = document.querySelector(SECURITY_ZONE_ICON);
+        const favicon_original = document.querySelector(ACTIVE_TAB);
+        const siteinfo = document.querySelector(SECURITY_ZONE);
+        if(!favicon_original || !sz_icon || !siteinfo){return;}
+        remove_existing_cloned_favicon();
+        // copy, style and set the favicon
+        // use deep clone to also copy svg icons
+        const favicon = favicon_original.cloneNode(true);
+        favicon.style.width = "16px";
+        favicon.style.height = "16px";
+        favicon.style.display = "block";
+        favicon.style.margin = "3px";
+        favicon.style.backgroundSize = "contain";
+        siteinfo.style.display = "flex";
+        sz_icon.parentNode.insertBefore(favicon, sz_icon);
     }
-}
 
-// Load the mod
-function load_favicon_security_mod(){
-    const browser = document.querySelector(BROWSER_STATE_INDICATOR)
-    const tab_change_indicator = document.querySelector(TAB_CHANGE_INDICATOR);
-    if (browser && tab_change_indicator) {
-        browserStateChangeObserver.observe(browser, BROWSER_STATE_INDICATOR_CONFIG);
-        differentTabActivatedObserver.observe(tab_change_indicator, TAB_CHANGE_INDICATOR_CONFIG);
-        clone_favicon_and_add_to_security();
-    } else {
-        setTimeout(load_favicon_security_mod, 100);
+    // Remove any existing cloned favicon
+    function remove_existing_cloned_favicon(){
+        const old_sz_favicon = document.querySelector(SECURITY_ZONE+" .favicon");
+        if(old_sz_favicon){
+            old_sz_favicon.parentElement.removeChild(old_sz_favicon);
+        }
     }
-}
 
-// BROWSER LOADED - entry point
-setTimeout(load_favicon_security_mod, 500);
+    // Load the mod
+    function load_favicon_security_mod(){
+        const browser = document.querySelector(".favicon");
+        if (browser) {
+            clone_favicon_and_add_to_security();
+        } else {
+            setTimeout(load_favicon_security_mod, 100);
+        }
+    }
+
+    // BROWSER LOADED - entry point
+    setTimeout(load_favicon_security_mod, 500);
+
+    // The tab was changed, so favicon needs changing
+    // this will cover changes to the .active tab AND
+    // cover changes to the favicon span thru subtree
+    chrome.tabs.onActivated.addListener(activeInfo => {
+        vivaldi.windowPrivate.getCurrentId(id => {
+            if(activeInfo.windowId === id){
+                clone_favicon_and_add_to_security();
+            }
+        });
+    });
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        if(changeInfo.faviconUrl || changeInfo.status){
+            clone_favicon_and_add_to_security();
+        }
+    });
+
+})();
