9d956461cadecfe12af66edea795ace72aec9b25
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-03-18 09:59:50 +0000

    #18 fix button machine, dont bother minifying

--- mods/button_machine.js
+++ mods/button_machine.js
@@ -16,7 +16,7 @@
     */
     const MY_BUTTONS = {
         TaskManager: {
-            html: `<button class="button-toolbar-small" title="Open Vivaldi Task Manager">
+            html: `<button draggable="false" tabindex="-1" title="Open Vivaldi Task Manager">
                 <svg viewBox="0 0 16 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9 8v2h-2v-2h-4v2h-2v-4h6v-2h-2v-4h6v4h-2v2h6v4h-2v-2h-4zm-9 2v3h4v-3h-4zm12 0v3h4v-3h-4zm-6 0v3h4v-3h-4z"></path>
                 </svg>
@@ -24,24 +24,8 @@
             onclick: () => {
                 vivaldi.utilities.openTaskManager(() => {});
             },
-            placeAfter: "#footer > div > div:nth-child(1)"
-        },
-
-        ActivateAllPanels: {
-            html: `<button class="button-toolbar-small" title="Activate all panels">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                    <path d="M4.5 4.5C5.4 3.6 6.6 3 8 3c2.8 0 5 2.2 5 5h2c0-3.9-3.1-7-7-7-1.9 0-3.7.8-4.9 2.1L1 1v6h6L4.5 4.5zM11.5 11.5c-.9.9-2.1 1.5-3.5 1.5-2.8 0-5-2.2-5-5H1c0 3.9 3.1 7 7 7 1.9 0 3.7-.8 4.9-2.1L15 15V9H9l2.5 2.5z"></path>
-                </svg>
-            </button>`,
-            onclick: () => {
-                const webPanels = document.querySelectorAll("#switch button.webviewbtn");
-                webPanels.forEach(button => {
-                    button.click();
-                });
-                webPanels[webPanels.length-1].click();
-            },
-            placeAfter: "#footer > div > div:nth-child(1)"
-        },
+            placeAfter: "#browser > .toolbar-statusbar > div:nth-child(10)"
+        }
     };
 
     /**
@@ -60,6 +44,7 @@
      */
     function makeButton(buttondef) {
         const div = document.createElement("div");
+        div.className = "button-toolbar";
         div.innerHTML = buttondef.html;
         const newBtn = div.firstChild;
         if(buttondef.onclick){
@@ -70,7 +55,7 @@
                 doContentScript(buttondef.contentScript);
             });
         }
-        return newBtn;
+        return div;
     }
 
     /**
