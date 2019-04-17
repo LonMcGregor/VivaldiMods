bad43d5101878f440b167e678ea47a01145753da
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-04-17 17:44:43 +0100

    panel search by tam and joao

--- mods/panel_actions.js
+++ mods/panel_actions.js
@@ -1,6 +1,8 @@
 /*
 * Panel Actions (A Mod for Vivaldi)
 * LonM.vivaldi.net
+* Search actions by tam710562 and joao.rossa on vivaldi.net
+* https://forum.vivaldi.net/topic/26623/zoom-other-page-actions-in-web-panels/
 * No Copyright Reserved
 */
 
@@ -19,6 +21,76 @@
     display: string of html - The innerHTML of the toolbar button
     display_class: string - One or more classes to give the button
     */
+
+    const ACTIONS_SEARCHTEXT = {
+        search_label :{
+            title: "Search label",
+            script: function(target, webview){
+
+            },
+            display:"<span>Find in panel:&nbsp;</span>",
+            display_class: "search-label"
+        },
+        search_input :{
+
+            title: "Search input",
+            script: function(target, webview){
+                target.addEventListener("input", function() {
+                    ACTIONS_SEARCHTEXT.value = target.value;
+                    webview.find(ACTIONS_SEARCHTEXT.value);
+                });
+                target.addEventListener("keypress", function(e) {
+                    if (e.keyCode == 13) {
+                        if (!e.shiftKey) {
+                            webview.find(ACTIONS_SEARCHTEXT.value);
+                        } else {
+                            webview.find(ACTIONS_SEARCHTEXT.value, { backward: true });
+                        }
+                    }
+                });
+            },
+            display:"<input type='search' id='panel-search-input'/>",
+            display_class: "search-input"
+        },
+
+        previous_match : {
+            title: "Previous match",
+            script: function(target, webview){
+                webview.find(ACTIONS_SEARCHTEXT.value, { backward: true });
+            },
+            display:`<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+				<path fill-rule="evenodd" d="M5 8L8.63432 4.36568C9.13829 3.86171 10 4.21865 10 4.93137L10 11.0686C10 11.7814 9.13829 12.1383 8.63431 11.6343L5 8Z"></path>
+			</svg>`,
+            display_class: "previous-match"
+        },
+
+        next_match : {
+            title: "Next match",
+            script: function(target, webview){
+                webview.find(ACTIONS_SEARCHTEXT.value);
+            },
+            display:`<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+			<path fill-rule="evenodd" d="M11 8L7.36568 11.6343C6.86171 12.1383 6 11.7814 6 11.0686L6 4.93137C6 4.21865 6.86171 3.86171 7.36569 4.36569L11 8Z"></path>
+			</svg>`,
+            display_class: "next-match"
+        },
+
+        cancel_search :{
+
+            title: "Cancel search",
+            script: function(target, webview){
+                webview.parentElement.parentElement.querySelector("#pamodsearchTextBar").remove();
+                webview.stopFinding("clear");
+                delete ACTIONS_SEARCHTEXT.value;
+            },
+            display:`<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+				<path d="M4.29289 4.29289C4.68342 3.90237 5.31658 3.90237 5.70711 4.29289L8 6.58579L10.2929 4.29289C10.6834 3.90237 11.3166 3.90237 11.7071 4.29289C12.0976 4.68342 12.0976 5.31658 11.7071 5.70711L9.41421 8L11.7071 10.2929C12.0976 10.6834 12.0976 11.3166 11.7071 11.7071C11.3166 12.0976 10.6834 12.0976 10.2929 11.7071L8 9.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L6.58579 8L4.29289 5.70711C3.90237 5.31658 3.90237 4.68342 4.29289 4.29289Z"></path>
+			</svg>`,
+            display_class: "cancel-search"
+
+        }
+    };
+
     const ACTIONS = {
 
         zoom_out: {
@@ -95,6 +167,16 @@
             <path d="m 10.425781,0 -5.5410155,4.96094 h -4.234375 v 4.74805 h 4.1074219 l 5.6679686,5.23047 z m 1.767579,1.50782 v 0.59961 c 0.628524,0 1.262682,0.52233 1.751953,1.49023 0.489269,0.96789 0.804686,2.34168 0.804686,3.86133 0,1.51964 -0.315417,2.89148 -0.804686,3.85937 -0.489271,0.9679 -1.123429,1.49219 -1.751953,1.49219 v 0.59961 c 0.949262,0 1.742407,-0.74276 2.287109,-1.82031 0.544702,-1.07756 0.869141,-2.52926 0.869141,-4.13086 0,-1.60161 -0.324439,-3.05525 -0.869141,-4.13281 -0.544702,-1.07755 -1.337847,-1.81836 -2.287109,-1.81836 z m -0.921875,1.97851 v 0.59961 c 0.378754,0 0.787693,0.3227 1.113281,0.9668 0.325586,0.64409 0.539061,1.57016 0.539061,2.5957 1e-6,1.02554 -0.213475,1.95161 -0.539061,2.5957 -0.325588,0.64409 -0.734527,0.9668 -1.113281,0.9668 v 0.59961 c 0.699491,0 1.267416,-0.54311 1.648436,-1.29687 0.381021,-0.75375 0.603517,-1.75774 0.603517,-2.86524 0,-1.1075 -0.222496,-2.11344 -0.603517,-2.86719 -0.38102,-0.75375 -0.948945,-1.29492 -1.648436,-1.29492 z"></path>
             </svg>`,
             display_class: "panel-action-mute"
+        },
+        search: {
+            title: "Search Text",
+            script: function(target, webview){
+                addSearchTextControls(webview);
+            },
+            display:`<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+			<path d="M944.448 934.656c-7.552 7.616-19.968 7.616-27.52 0l-124.416-124.352c10.368-8 20.096-16.64 29.12-25.984l122.816 122.752C952.064 914.624 952.064 926.976 944.448 934.656zM638.72 823.616c-118.4 0-214.336-95.872-214.336-214.272 0-118.336 95.936-214.272 214.336-214.272 118.336 0 214.272 95.936 214.272 214.272C852.992 727.744 757.056 823.616 638.72 823.616zM638.72 434.112c-96.832 0-175.36 78.528-175.36 175.296 0 96.832 78.528 175.296 175.36 175.296 96.832 0 175.296-78.528 175.296-175.296C814.08 512.576 735.552 434.112 638.72 434.112zM210.176 745.728l0-38.976 194.752 0c5.696 13.696 12.672 26.624 20.608 38.976L210.176 745.728zM210.176 628.864 210.176 589.888l176.256 0C385.984 596.352 385.472 602.816 385.472 609.344c0 6.656 0.512 13.056 0.96 19.52L210.176 628.864zM210.176 356.16l194.752 0 0 38.912L210.176 395.072 210.176 356.16zM404.992 512 404.992 512 210.176 512 210.176 473.024l194.752 0L404.928 512zM736.128 356.16 638.72 356.16l0 0L638.656 356.16 580.288 356.16c-43.072 0-77.952-34.88-77.952-77.952L502.336 102.976 151.744 102.976c-21.504 0-38.912 17.408-38.912 38.912l0 740.224c0 21.504 17.408 38.848 38.912 38.848l545.408 0c21.504 0 38.912-17.344 38.912-38.848l0-38.976c7.936-3.328 15.68-6.976 23.232-11.008l15.68 15.744 0 34.24c0 43.008-34.88 77.888-77.888 77.888L151.744 960c-43.008 0-77.888-34.88-77.888-77.888L73.856 141.888C73.856 98.88 108.736 64 151.744 64c0 0 149.632 0 292.16 0C447.424 64 450.88 64 454.4 64c6.784 0 13.376 0 20.096 0l54.464 0 240.448 258.944-1.216 6.848 6.848 6.848 0 8.704 0 49.728 0 1.152c-12.288-7.936-25.344-14.912-38.912-20.608L736.128 356.16zM541.312 135.36l0 142.912c0 21.504 17.408 38.976 38.976 38.976l131.584 0L541.312 135.36z" />
+			</svg>`,
+            display_class: "panel-action-search-text"
         }/*,
 
         template: {
@@ -106,6 +188,40 @@
             display_class: ``
         },*/
     };
+
+
+
+    function addSearchTextControls(webview){
+        const panel = webview.parentElement.parentElement;
+        const alreadyAdded = panel.querySelector("#pamodsearchTextBar");
+        if(alreadyAdded){return;}
+
+        const searchTextBar = document.createElement("div");
+        searchTextBar.id = "pamodsearchTextBar";
+        searchTextBar.className = "toolbar toolbar-medium toolbar-statusbar";
+        for(const key in ACTIONS_SEARCHTEXT){
+            const action = ACTIONS_SEARCHTEXT[key];
+            let newButton;
+            if(key === "value") {
+                continue;
+            } else if(key === "search_label" || key === "search_input") {
+                newButton = createElement(action, webview);
+            } else {
+                newButton = createActionButton(action, webview);
+            }
+            searchTextBar.appendChild(newButton);
+        }
+        panel.insertBefore(searchTextBar,panel.querySelector("footer"));
+    }
+
+    function createElement(action, webview){
+        const template = document.createElement("template");
+        template.innerHTML = action.display.trim();
+        const element = template.content.firstChild;
+        action.script(element, webview);
+        element.title = action.title;
+        return element;
+    }
 
     /**
      * Change the zoom
@@ -178,7 +294,7 @@
         if(alreadyAdded){return;}
         const footer = document.createElement("footer");
         const footerToolbar = document.createElement("div");
-        footerToolbar.className = "toolbar";
+        footerToolbar.className = "toolbar toolbar-medium toolbar-statusbar";
         footer.appendChild(footerToolbar);
         const webview = panel.querySelector("webview");
         for(const key in ACTIONS){
