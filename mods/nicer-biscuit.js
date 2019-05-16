89fc3d705137c4b022e2c21ab46b3e20ccfe196c
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-05-16 19:46:48 +0100

    biscuit auto-copy version

--- mods/nicer-biscuit.js
+++ mods/nicer-biscuit.js
@@ -3,10 +3,16 @@
     "use strict";
 
     function doBiscuit(){
-        document.querySelector(".toolbar-mainbar").insertBefore(document.querySelector(".biscuit-setting-version"), document.querySelector(".toolbar-extensions"));
-        document.querySelector(".biscuit-setting-version").style.paddingRight = "0";
-        document.querySelector(".biscuit-string").size = "20";
-        document.querySelector(".biscuit-string").style = "-webkit-mask-image: -webkit-linear-gradient(180deg, transparent 0, #fff 12px);-webkit-mask-position: top right";
+        const biscuit = document.querySelector(".biscuit-string");
+        const biscuitContainer = document.querySelector(".biscuit-setting-version");
+        const biscuitLogo = document.querySelector(".application-icon-biscuit");
+        document.querySelector(".toolbar-mainbar").insertBefore(biscuitContainer, document.querySelector(".toolbar-extensions"));
+        biscuitContainer.style.paddingRight = "0";
+        biscuit.size = "13";
+        biscuitLogo.addEventListener("click", () => {
+            document.querySelector(".biscuit-string").select();
+            document.execCommand("copy");
+        });
     }
 
     setTimeout(function wait() {
